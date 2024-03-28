901d54ded7325d2d19569c2d2ec4cf4353c3c551
blueheeya83 <blueheeya12@gmail.com>
2024-03-28 13:17:26 +0900

    first commit

---
+++ js/cardData.js
@@ -0,0 +1,37 @@
+const carddataType1 = [
+    {
+        img:"",
+        title:"test",
+        content: "askdlsfjsld"
+    },
+    {
+        img:"",
+        title:"test",
+        content: "askdlsfjsld"
+    },
+]
+const carddataType2 = [
+    {
+        img:"",
+        title:"test",
+        content: "askdlsfjsld"
+    },
+    {
+        img:"",
+        title:"test",
+        content: "askdlsfjsld"
+    },
+]
+const carddataType3 = [
+    {
+        img:"",
+        title:"test",
+        content: "askdlsfjsld"
+    },
+    {
+        img:"",
+        title:"test",
+        content: "askdlsfjsld"
+    },
+]
+export {carddataType1,carddataType2,carddataType3};
